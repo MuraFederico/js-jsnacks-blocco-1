382c02b2cb9a2c60e3439f5291788f18d56c95ac
MuraFederico <federicomura98@gmail.com>
2022-02-25 15:15:12 +0100

    es 1

---
+++ script.js
@@ -0,0 +1,14 @@
+// es 1.1
+
+const num1 = parseFloat(prompt('numero 1'));
+const num2 = parseFloat(prompt('numero 2'));
+
+if (num1 > num2) {
+    alert(num1);
+
+}else if (num2 > num1){
+    alert(num2);
+
+}else{
+    alert('i numeri sono uguali');
+}
